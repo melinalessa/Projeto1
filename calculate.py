16a1b28039f9f889bab859d51a52e6a0a1737559
melinalessa <lessamelina@gmail.com>
2023-03-30 19:12:44 -0300

    programando dia 2

--- calculate.py
+++ calculate.py
@@ -1,1 +1,16 @@
-print(10)
+import tkinter as tk
+
+calculate = ""
+
+def add_to_calculation(symbol):
+    pass
+
+def evaluate_calculation():
+    pass
+
+def clear_field():
+    pass
+
+root = tk.Tk()
+root.geometry("300x275")
+root.mainloop()
